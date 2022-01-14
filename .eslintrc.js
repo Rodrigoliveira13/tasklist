72629a5cc0b84edeed4103fc14654934d1d8573e
rodrigooliveira <rodrigo.oliveirags14@gmail.com>
2022-01-14 15:15:25 +0000

    Configurações iniciais do projeto

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+const { next } = require("sucrase/dist/parser/tokenizer");
+
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier'
+  ],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  rules: {
+    "prettier/prettier" : "error",
+    "class-methods-use-this" : "off",
+    "no-param-reassign" : "off",
+    "camelcase" : "off",
+    "no-underscore-dangle" : "off",
+    "no-unused-vars" : ["error", {"argsIgnorePattern": "next"}]
+  },
+};
